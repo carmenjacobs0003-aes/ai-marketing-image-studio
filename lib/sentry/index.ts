a9f002e47e761e9cd63a81f10f32b621376e8f40
carmenjacobs0003-aes <carmenjacobs0003@gmail.com>
2026-05-11 23:44:37 +0100

    Update index.ts

--- lib/sentry/index.ts
+++ lib/sentry/index.ts
@@ -1,19 +1,3 @@
-import * as Sentry from "@sentry/nextjs";
-import { env } from "@/lib/env";
-
 export function registerSentry() {
-  Sentry.init({
-    dsn: env.SENTRY_DSN ?? env.NEXT_PUBLIC_SENTRY_DSN,
-    environment: process.env.VERCEL_ENV ?? process.env.NODE_ENV,
-    release: process.env.VERCEL_GIT_COMMIT_SHA,
-    tracesSampleRate: env.SENTRY_TRACES_SAMPLE_RATE,
-    profilesSampleRate: env.SENTRY_PROFILES_SAMPLE_RATE,
-    beforeSend(event) {
-      if (event.request?.cookies) {
-        delete event.request.cookies;
-      }
-
-      return event;
-    }
-  });
+  return;
 }
